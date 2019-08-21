cce50e2acb5748bdd1674f9452666b7103531938
吴福增 <wufuzeng_lucky@sina.com>
2019-08-21 08:58:23 +0800

    wfz

---
+++ AspectsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AspectsDemo
+//
+//  Created by 吴福增 on 2019/8/21.
+//  Copyright © 2019 吴福增. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AspectsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AspectsDemo
+//
+//  Created by 吴福增 on 2019/8/21.
+//  Copyright © 2019 吴福增. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
